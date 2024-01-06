9a297af00ffc33ca3cf2252b70748fb059817899
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-05 21:23:23 -0300

    Add files via upload

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -447,7 +447,7 @@ class Paths
 
 	#if MODS_ALLOWED
 	inline static public function mods(key:String = '') {
-		return 'mods/' + key;
+		return #if mobile Sys.getCwd() + #end 'mods/' + key;
 	}
 
 	inline static public function modsFont(key:String) {
@@ -505,7 +505,7 @@ class Paths
 			if(FileSystem.exists(fileToCheck))
 				return fileToCheck;
 		}
-		return 'mods/' + key;
+		return #if mobile Sys.getCwd() + #end 'mods/' + key;
 	}
 	#end
 
